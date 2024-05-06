b24cdc56a853837a07deff70a5119d035d9da91d
Brody Stotts <150973033+bdstotts@users.noreply.github.com>
2024-05-06 16:31:08 -0600

    add logic for winning; add color selection for player squares

--- tic.js
+++ tic.js
@@ -1,5 +1,10 @@
 function game(){
 
+    player1Color = "#0000ff"
+    player2Color = "#ffa500"
+
+    let turn = 1;
+
     function createBoard(){
        squares = [];
        for(i=0;i<9;i++){
@@ -12,8 +17,83 @@ function game(){
         createSquare()
        }
     }
-
     createBoard();
+
+
+    //const pickers = document.querySelectorAll("pickers")
+   const player1Picker = document.getElementById("picker1");
+    const player2Picker = document.getElementById("picker2");
+
+
+    player1Picker.addEventListener("change", pickColor1)
+    player2Picker.addEventListener("change", pickColor2)
+    
+
+    function pickColor1(){
+        console.log("Player 1 color changed")
+        player1Color = this.value;
+        this.disabled = true;
+    }
+
+    function pickColor2(){
+        console.log("Player 2 color changed")
+        player2Color = this.value;
+        this.disabled = true;
+    }
+
+
+
+
+    squares.forEach(element => {
+        element.addEventListener("click", placePiece)
+        //element.style.backgroundColor = "#ffffff"
+
+        function placePiece(){
+
+            console.log("piece has been placed");
+            if(turn%2 ===1){
+                element.style.backgroundColor = player1Color;
+            }
+            else{
+                element.style.backgroundColor = player2Color;
+            }
+            turn+=1;
+            element.classList.add("clicked")
+
+
+            console.log(turn)
+            console.log(turn%2)
+
+            zero = squares[0].style.backgroundColor;
+
+            function win(x,y,z){
+
+                 if((squares[x].style.backgroundColor === squares[y].style.backgroundColor)
+            &&
+            (squares[x].style.backgroundColor === squares[z].style.backgroundColor)
+            && 
+            (squares[x,y,z].classList.contains("clicked")))
+                {
+                    alert("Player Wins" + zero)}
+            }
+
+            win(0,1,2)
+            win(3,4,5)
+            win(6,7,8)
+            win(0,3,6)
+            win(1,4,7)
+            win(2,5,8)
+            win(0,4,8)
+            win(2,4,6)      
+                
+        }
+
+           
+        
+    });
+
+
+   
 
 
 }
