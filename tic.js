2b502fb0bac37d02f87984461b6a52d5ca3003c3
Brody Stotts <150973033+bdstotts@users.noreply.github.com>
2024-05-07 11:43:52 -0600

    colors can no longer be changed after placing a piece; players can't override other squares

--- tic.js
+++ tic.js
@@ -32,13 +32,13 @@ function game(){
     function pickColor1(){
         console.log("Player 1 color changed")
         player1Color = this.value;
-        this.disabled = true;
+        //this.disabled = true;
     }
 
     function pickColor2(){
         console.log("Player 2 color changed")
         player2Color = this.value;
-        this.disabled = true;
+        //this.disabled = true;
     }
 
 
@@ -46,48 +46,50 @@ function game(){
 
     squares.forEach(element => {
         element.addEventListener("click", placePiece)
-        //element.style.backgroundColor = "#ffffff"
-
-        function placePiece(){
-
-            console.log("piece has been placed");
-            if(turn%2 ===1){
-                element.style.backgroundColor = player1Color;
-            }
-            else{
-                element.style.backgroundColor = player2Color;
-            }
-            turn+=1;
-            element.classList.add("clicked")
-
-
-            console.log(turn)
-            console.log(turn%2)
-
-            zero = squares[0].style.backgroundColor;
-
-            function win(x,y,z){
-
-                 if((squares[x].style.backgroundColor === squares[y].style.backgroundColor)
-            &&
-            (squares[x].style.backgroundColor === squares[z].style.backgroundColor)
-            && 
-            (squares[x,y,z].classList.contains("clicked")))
-                {
-                    alert("Player Wins" + zero)}
-            }
-
-            win(0,1,2)
-            win(3,4,5)
-            win(6,7,8)
-            win(0,3,6)
-            win(1,4,7)
-            win(2,5,8)
-            win(0,4,8)
-            win(2,4,6)      
                 
-        }
 
+         function placePiece(){
+             if(!element.classList.contains("clicked")){
+                 console.log("piece has been placed");
+                 if(turn%2 ===1){
+                     element.style.backgroundColor = player1Color;
+                 }
+                 else{
+                     element.style.backgroundColor = player2Color;
+                 }
+                 turn+=1;
+                 element.classList.add("clicked")
+                 console.log(turn)
+                 console.log(turn%2)
+                
+                 function win(x,y,z){
+                      if((squares[x].style.backgroundColor === squares[y].style.backgroundColor)
+                 &&
+                 (squares[x].style.backgroundColor === squares[z].style.backgroundColor)
+                 && 
+                 (squares[x,y,z].classList.contains("clicked")))
+                     {
+                         alert("Player Wins")}
+                 }
+                 win(0,1,2)
+                 win(3,4,5)
+                 win(6,7,8)
+                 win(0,3,6)
+                 win(1,4,7)
+                 win(2,5,8)
+                 win(0,4,8)
+                 win(2,4,6)      
+
+            }
+
+            if(turn>1){
+                player1Picker.disabled = true;
+            }
+        
+            if(turn>2){
+                player2Picker.disabled = true;
+            }
+        }
            
         
     });
